38ea9362b0f92131a754e00e126197da98e9ad32
Henry Khaung <henrykhaung@berkeley.edu>
2023-06-19 17:45:27 -0700

    added functionality to buttons and links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -24,7 +25,8 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
